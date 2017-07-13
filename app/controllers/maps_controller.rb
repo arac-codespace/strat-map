7831134b884d724610954b2b4880d334363dab02
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-07-13 05:36:10 +0000

    Implemented map markers, infowindows and the texture render condition. May opt to move the columns out of google's infowindow...

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,9 +1,25 @@
 class MapsController < ApplicationController
   
   def index
-  end
-  
-  
-  
+    
+    @user_id = current_user.id
+
+    # Find all strat columns
+    @strat = StratColumn.where(user_id: @user_id)
+    
+    @textures_to_render = []
+    
+    # Loop to find urls to render
+    @strat.each do |column|
+      @layers = Layer.joins(:strat_column).where(strat_columns: {id: column.id}).each do |layer|
+        temp = "svg/#{layer.lithology.rock_type.downcase}/#{layer.lithology.url}"
+        
+        if !@textures_to_render.include?(temp)
+          @textures_to_render << temp
+        end # if end
+        
+      end #layer inner loop ned
+    end #strat outer loop end
+  end # def end
   
 end
