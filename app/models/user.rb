c45160a5d416cfc691475f3d2dcb307d7b2ad2b6
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-07-18 02:00:31 +0000

    Added some temporary user registration validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   has_many :strat_columns
   
-  
+  # See Ruby Regexp
+  validates_format_of :email, with: /\b[A-Z0-9._%a-z\-]+@stratmap\z/, message: "Add @stratmap next to your username."
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable 
+        # :recoverable, 
 end
