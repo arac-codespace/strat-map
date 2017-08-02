5bd74eb10b5cc4f44333260e18237525ff38fc9d
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-08-02 03:23:58 +0000

    Ooops. Migration typo

--- db/migrate/20170715223245_rename_contact_column_layers.rb
+++ db/migrate/20170715223245_rename_contact_column_layers.rb
@@ -1,6 +1,6 @@
 class RenameContactColumnLayers < ActiveRecord::Migration[5.1]
   def change
     rename_column :layers, :contact, :contact_id
-    change_column :layers, :contact_id, 'integer USING CAST(column_name AS integer)'
+    change_column :layers, :contact_id, 'integer USING CAST(contact_id AS integer)'
   end
 end
