2efd48c7edf809172984a0d8cca4ccd5f41f2566
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-08-13 19:50:05 +0000

    Added response for html for those buttons in charge of ajax responses. See layers controller

--- app/controllers/layers_controller.rb
+++ app/controllers/layers_controller.rb
@@ -7,21 +7,25 @@ class LayersController < ApplicationController
     # @current_user = current_user
     # @current_user_id = @current_user.id
     @strat_column_id = StratColumn.find(params[:id])
+    respond_to do |format|
+      format.html {redirect_to edit_strat_column_path(@strat_column_id)}
+      format.js
+    end
+      
   end  
 
 
   def create
     @layer = Layer.create(layer_params)
+    respond_to do |format|
       if @layer.save
-        redirect_to strat_column_path(id: @layer.strat_column_id)
+        format.js {redirect_to strat_column_path(id: @layer.strat_column_id)}
       else
-        # Basically, if creation fails, I want you to respond with
-        # a json message which I will process with js (application.js).
-        respond_to do |format|
-          format.json {render json: @layer.errors.full_messages, status: :unprocessable_entity}
-        end
-  
-      end      
+      # Basically, if creation fails, I want you to respond with
+      # a json message which I will process with js (application.js).
+        format.json {render json: @layer.errors.full_messages, status: :unprocessable_entity}
+      end
+    end      
   end
   
   
@@ -41,7 +45,10 @@ class LayersController < ApplicationController
   
   def edit
     @layer = Layer.find_by(id: params[:id])
-    # @user_id = params[:id]
+    respond_to do |format|
+      format.js
+      format.html {redirect_to edit_strat_column_path(@strat_column_id)}
+    end
   end  
   
   
@@ -49,17 +56,14 @@ class LayersController < ApplicationController
     # Find the corresponding record id that you want to update!
     
     @layer = Layer.find_by(id: params[:id])
-    
-    if @layer.update(layer_params)
-      redirect_to strat_column_path(id: @layer.strat_column_id)
-    else
-      respond_to do |format|
+    respond_to do |format|
+      if @layer.update(layer_params)
+        format.js {redirect_to strat_column_path(id: @layer.strat_column_id)}
+      else
         format.json {render json: @layer.errors.full_messages, status: :unprocessable_entity}
       end
     end
   end   
-  
-  
   
   def layer_params
     params.require(:layer).permit(:lithology_name, :timescale_name, :interval_name ,:id, :strat_column_id, :lithology_id, :timescale_id, :contact_id, :epoch_age, :contact, :_destroy, :name, :name2, :name3, :formation, :thickness, :description )
