e4eb948223f9fb08d0d4ed064e59784b4da85633
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-09-01 04:55:19 +0000

    Basic route setup for collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,8 @@
 class CollectionsController < ApplicationController
   
   def index
+    @user_id = current_user
+    @collections = Collection.all
   end
   
   def new
@@ -14,13 +16,41 @@ class CollectionsController < ApplicationController
    @collection = Collection.new(collection_params)
     if @collection.save
         flash[:notice] = "Collection created succesfully"
-        redirect_to collections_path
+        redirect_to collection_path(id: @collection)
     else
         @id = current_user.id
         flash[:error] = @collection.errors.full_messages
         render action: :new
     end        
   end
+  
+  def show
+    @collection = Collection.find(params[:id])
+  end
+  
+  def edit
+    @collection = Collection.find(params[:id])
+    @user_id = current_user
+    @user_columns = StratColumn.where(user_id: @user_id)    
+  end
+  
+  def update
+    @collection = Collection.find(params[:id])
+    if @collection.update_attributes(collection_params)
+      redirect_to collection_path(id: @collection)
+    else
+      render action: :new
+    end
+
+  end
+
+  def destroy
+    @collection = Collection.find(params[:id])
+    @collection.destroy
+    # Redirect to strats index
+    redirect_to collections_path
+  end  
+
   
   private
   def collection_params
