4385930f078f0a8260391f755fa8312329fffb72
arac-codespace <alex.aguirre0026@gmail.com>
2018-01-30 01:22:20 -0400

    Updated uglifier and edited compressor settings to allow ES6 on deploy to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # Enables ES6 deploy to heroku
+  config.assets.js_compressor = Uglifier.new(harmony: true) 
+
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
