b10ac7ec1d35bab4285cd2f69032d8ef42de0bb2
arac-codespace <alex.aguirre0026@gmail.com>
2018-02-08 15:01:45 -0400

    Hmm, discovered sublimelint and cleaned up data controller with it.

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,46 +1,47 @@
+# frozen_string_literal: true
+
+# Controller dedicated to rendering json data
 class DataController < ApplicationController
-before_action :authenticate_user!
-  
+  before_action :authenticate_user!
+
   # /strat_column/:strat_column_id/data
   def data
-    @strat_column_id = params[:strat_column_id]
-    @layers = Layer.where(strat_column_id: @strat_column_id).order("created_at DESC")
-        
+    strat_column_id = params[:strat_column_id]
+    layers = Layer.where(strat_column_id: strat_column_id).order('created_at DESC')
+    except_attr = %i[created_at updated_at]
+    except = { except: except_attr }
+    only = { only: %i[interval_name color abbrev] }
     # https://stackoverflow.com/questions/17730121/include-associated-model-when-rendering-json-in-rails
-    render(json: @layers, except: [:created_at, :updated_at], include: [{strat_column: {except: [:created_at, :updated_at]}},{lithology: {except: [:created_at, :updated_at]}}, {timescale: {only: [:interval_name, :color, :abbrev]}}, {contact: {except: [:created_at, :updated_at]}}, {fossils: {except: [:created_at, :updated_at]}}] )
-    
+    render(
+      json: layers, except: except_attr,
+      include: [{ strat_column: except }, { lithology: except },
+                { timescale: only }, { contact: except }, { fossils: except }]
+    )
   end
-  
+
   def all_data
-  
-    @user_id = current_user.id
-    @strat_columns = StratColumn.where(user_id: @user_id).where.not(lat: nil).where.not(lng: nil)
-        
+    user_id = current_user.id
+    strat_columns = StratColumn.where(user_id: user_id).where.not(lat: nil).where.not(lng: nil)
     # https://stackoverflow.com/questions/17730121/include-associated-model-when-rendering-json-in-rails
-    render :json => @strat_columns, :except => [:created_at, :updated_at]  
-  
+    render json: strat_columns, except: %i[created_at updated_at]
   end
-  
-  
+
   def timescales_data
-    
-    @timescales = Timescale.order(:interval_name).where("interval_name ilike ?", "%#{params[:term]}%")
-    render :json => @timescales.map(&:interval_name)
-    
+    timescales = Timescale.order(:interval_name).where('interval_name ilike ?', "%#{params[:term]}%")
+    render json: timescales.map(&:interval_name)
   end
-  
+
   def lithologies_data
-    @lithologies = Lithology.where('name ILIKE :search OR name2 ILIKE :search OR name3 ILIKE :search OR url LIKE :search', search: "%#{params[:term]}%")
-    render :json => @lithologies.map(&:name_with_texture_num)
+    query = 'name ILIKE :search OR name2 ILIKE :search OR name3 ILIKE :search OR url LIKE :search'
+    lithologies = Lithology.where(query, search: "%#{params[:term]}%")
+    render json: lithologies.map(&:name_with_texture_num)
   end
-  
-  def collections_data  
-    @collections_id = params[:collection_id]
-    @collection_columns = Collection.find(@collections_id).strat_columns
-    
+
+  def collections_data
+    collections_id = params[:collection_id]
+    collection_columns = Collection.find(collections_id).strat_columns
     # https://stackoverflow.com/questions/17730121/include-associated-model-when-rendering-json-in-rails
-    @exceptions = [:created_at, :updated_at]
-    render(json: @collection_columns, except: @exceptions, methods: [:total_thickness] )
+    exceptions = %i[created_at updated_at]
+    render(json: collection_columns, except: exceptions, methods: [:total_thickness])
   end
-  
 end
