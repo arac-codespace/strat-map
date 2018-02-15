152281d766d051d662a299e28d8cc752ca2542a6
arac-codespace <alex.aguirre0026@gmail.com>
2018-02-15 00:34:39 -0400

    Refactored routes.  Preparing to install html-tidy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
+# Routes
 Rails.application.routes.draw do
-  
   devise_for :users
   root to: 'pages#home'
-  # get 'data/stratdata', to: 'data#stratdata'
-  
+
   get '/data', to: 'data#all_data'
   get '/timescales', to: 'data#timescales_data'
   get '/lithologies', to: 'data#lithologies_data'
@@ -11,13 +12,11 @@ Rails.application.routes.draw do
     get '/data', to: 'data#data'
     resources :layers
   end
-  
+
   resources :maps, only: [:index]
-  # resources :layers
   resources :collections do
     get '/collections', to: 'data#collections_data'
   end
-  
-  post '/collection_sort', to: 'collections#sort'
 
+  post '/collection_sort', to: 'collections#sort'
 end
